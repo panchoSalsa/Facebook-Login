11ffff0f7f3d51a6e34ecc79e7921d315eb1d05a
Franco <frfranco@uci.edu>
2016-09-06 19:12:18 -0700

    succesfully retrieved my birthday and email

--- app/src/main/java/com/example/franciscofranco/mylogin/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/mylogin/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.franciscofranco.mylogin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private TextView info;
@@ -27,30 +36,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("user_birthday", "email"));
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                info.setText(
-                        "User ID: "
-                                + loginResult.getAccessToken().getUserId()
-                                + "\n" +
-                                "Auth Token: "
-                                + loginResult.getAccessToken().getToken()
-                );
+//                info.setText(
+//                        "User ID: "
+//                                + loginResult.getAccessToken().getUserId()
+//                                + "\n" +
+//                                "Auth Token: "
+//                                + loginResult.getAccessToken().getToken()
+//                );
+
+                greeting();
 
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt canceled.");
+
+//                info.setText("Login attempt canceled.");
             }
 
             @Override
             public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
+//                info.setText("Login attempt failed.");
             }
         });
+
     }
 
     @Override
@@ -58,6 +72,47 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+
+    public void greeting() {
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        parseJSON(object);
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,birthday,name,email");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    public void parseJSON(JSONObject obj) {
+        Log.d("FRANCO_DEBUG", obj.toString());
+
+        String name = "" , birthday = "", email = "";
+
+        try {
+            name = obj.getString("name");
+            birthday = obj.getString("birthday");
+            email = obj.getString("email");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Welcome back " + name + "!\n");
+        sb.append("Your birthday is " + birthday + ".\n");
+        sb.append("Your registered email is " + email + ".\n");
+
+        info.setText(sb.toString());
+
+    }
 
 }
 
